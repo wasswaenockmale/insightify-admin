a38252b146f636399433fc0cc83b53b226622e4e
David Wampamba <davidwampamba@gmail.com>
2024-01-10 15:30:54 +0300

    Cross origin domains

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,10 +6,12 @@ module.exports = [
     config: {
       enabled: true,
       origin: [
+        "http://localhost:3000",
         "http://localhost:1336",
         "https://insightify-admin.ablestate.cloud",
         "https://ablestate.africa",
         "https://www.ablestate.africa",
+        "https://be.ablestate.cloud",
       ],
     },
   },
